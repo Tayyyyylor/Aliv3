b481cc94154dae987a3627c711b3ca5c59a7c698
Bryan Houblon <bryhoubl@WKMZT6DEB337.global.publicisgroupe.net>
2023-12-29 21:32:10 +0100

    format mobile page accueil

--- src/components/templates/Homepage/Homepage.js
+++ src/components/templates/Homepage/Homepage.js
@@ -1,36 +1,49 @@
-import React, {  useState } from 'react';
+import React, { useEffect} from 'react';
 import DefaultButton from '@/components/atoms/buttons/defaultButton/DefaultButton';
 import { useRouter } from 'next/router';
 
 export default function HomepageTemplate() {
-  // const cld = new Cloudinary({cloud: {cloudName: 'dna8sibxu'}});
-  const [showButton, setShowButton] = useState(false);
   const router = useRouter()
+
+
+// const isMobile = window.innerWidth < 1024
+
 
 const handleExplore = () => {
   router.push("/films")
 }
 
-const handleMouseMove = () => {
-  setShowButton(true);
-};
-const handleMouseLeave = () => {
-  setShowButton(false);
-};
+useEffect(() => {
+  // Vérifie si l'utilisateur est sur un appareil mobile lors du chargement initial
+  const checkMobile = () => {
+    if (window.innerWidth < 1024) {
+      router.push("/films");
+    }
+  };
+
+  // Appeler la fonction de vérification lors du chargement initial
+  checkMobile();
+
+  // Ajouter un écouteur d'événement de redimensionnement pour effectuer la vérification en cas de redimensionnement de la fenêtre
+  window.addEventListener('resize', checkMobile);
+
+  // Retirer l'écouteur d'événement lors du démontage du composant pour éviter les fuites de mémoire
+  return () => {
+    window.removeEventListener('resize', checkMobile);
+  };
+}, [router]);
+
 
   return (
+    
     <>
-        <div className='intro' onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+        <div className='intro'>
           <div className='intro__video'>
           <video className='video'  preload='auto' muted autoPlay controls>
   <source src="/reeelWebm.webm" type="video/webm" />
   </video>
           </div>  
-          {
-            showButton && (
               <DefaultButton label="Explore now ▶" onClick={handleExplore} className="intro__button"/>
-            )
-          }  
         </div>
 
 
