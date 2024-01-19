204bd380e44ee72e78772277dc3bda1b852b6dd3
Tayyyyylor <bryan.h.pro@gmail.com>
2024-01-19 22:15:25 +0100

    zoom photos dans modal images

--- src/components/molecules/imageModal/ImageModal.jsx
+++ src/components/molecules/imageModal/ImageModal.jsx
@@ -1,9 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
+import Backdrop from '@/components/atoms/backdrop/Backdrop'
 import CloseButton from '@/components/atoms/buttons/closeButton/CloseButton'
 import Title from '@/components/atoms/title/Title'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ImageModal({ label, onClick, gallery }) {
+
+  const [zoom, setZoom] = useState(false)
+  const [zoomIndex, setZoomIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setZoom(true)
+    setZoomIndex(index);
+  }
+
+  const handleClose = () => {
+    setZoom(false)
+    setZoomIndex(null)
+  }
+  console.log('zoomIndex', zoomIndex)
+
   return (
     <section className="imageModal">
       <Title label={label} className="imageModal__title" />
@@ -12,13 +28,27 @@ export default function ImageModal({ label, onClick, gallery }) {
         {gallery.map((src, index) => (
           <img
             key={index}
+            onClick={() => handleClick(index)}
             src={src}
             sizes="100vw"
             alt={`Photo ${index}`}
             className="imageModal__img"
           />
-        ))}
+          ))}
       </div>
+      {zoom && (
+        <>
+        <section className='imageModal__zoom'>
+      <img
+        src={gallery[zoomIndex]}
+        sizes="100vw"
+        alt={`Zoomed photo ${zoomIndex}`}
+        className="imageModal__zoom_img"
+        />
+        </section>
+      <Backdrop onCancel={handleClose} />
+        </>
+      )}
     </section>
   )
 }
