984868cc54dd3f83671a59c826ba2a24d1769a7e
Tayyyyylor <bryan.h.pro@gmail.com>
2024-01-22 00:20:04 +0100

    new homepage

--- src/components/atoms/logo/Logo.jsx
+++ src/components/atoms/logo/Logo.jsx
@@ -1,27 +1,11 @@
-// import { useEffect, useState } from "react";
 import Link from 'next/link'
 
-export default function Logo() {
-  // const [isMobile, setIsMobile] = useState(false);
-
-  // useEffect(() => {
-  //   // Fonction pour détecter si la vue est en mode mobile ou non
-  //   const handleResize = () => {
-  //     setIsMobile(window.innerWidth < 920);
-  //   };
-
-  //   window.addEventListener("resize", handleResize);
-
-  //   handleResize();
-
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, []);
+export default function Logo({ className }) {
+ 
 
   return (
     <Link href="/">
-      <h1 className="logo"> ali bensaali</h1>
+      <h1 className={`logo ${className}`}> ali bensaali</h1>
     </Link>
   )
 }
--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className="header">
-      <Logo />
+      <Logo className="header__logo"/>
       <Navbar />
     </header>
   )
