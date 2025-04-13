4b1a7bb847ace027fe501bcd3df28b7e40b4af46
Tayyyyylor <bryan.h.pro@gmail.com>
2025-04-13 20:49:56 +0200

    add button close

--- src/components/atoms/buttons/closeButton/CloseButton.jsx
+++ src/components/atoms/buttons/closeButton/CloseButton.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export default function  CloseButton({ className, onClick }) {
+
+  const handleClick = (e) => {
+    e.stopPropagation() // ⛔ empêche la remontée vers <main>
+    onClick?.(e)        // ✅ exécute la fermeture
+  }
   return (
     <div className={className}>
-      <span className="closeButton" onClick={onClick}>
+      <span className="closeButton" onClick={handleClick}>
         &times;
       </span>
     </div>
